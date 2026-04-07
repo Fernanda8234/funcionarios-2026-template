29a07f6336626c623d1d57e5da3454a2fc2fac06
FERNANDA CARDOSO DA MOTA <25203628@CFP127ALUNO.LOCAL>
2026-04-07 08:40:02 -0300

    feat: funcionários

--- app.js
+++ app.js
@@ -2,3 +2,36 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+function criarCard(funcionario){
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+    foto.src = `./img/${funcionario.imagem}`
+    foto.alt = `Foto de ${funcionario.nome}`
+
+    const nome = document.createElement('h3')
+    nome.textContent = funcionario.nome
+
+    const cargo = document.createElement('span')
+    cargo.textContent = funcionario.cargo
+
+    card.append(foto, nome, cargo)
+
+    // console.log(funcionario)
+    // return 1
+    // return '<h3>'
+    // funcionario.nome
+
+    return card
+}
+const cards = funcionarios.map(criarCard)
+console.log(cards)
+// filter
+// reduce
+
+//document.getElementById('container').append(...cards)
+document.getElementById('container').replaceChildren(...cards)
+
+
+
